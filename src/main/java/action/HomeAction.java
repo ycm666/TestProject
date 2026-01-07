3c9f0662b094784d5b3fc9b195869f9a627723dc
ycm666 <ycm666@naver.com>
2026-01-07 20:57:13 +0900

    second commit

---
+++ src/main/java/action/HomeAction.java
@@ -0,0 +1,28 @@
+package action;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class HelloAction
+ */
+@WebServlet("/")
+public class HomeAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		RequestDispatcher disp = request.getRequestDispatcher("home.jsp");
+		disp.forward(request, response);
+	}
+
+}
