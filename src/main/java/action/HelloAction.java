e0a5e2007e951300e9b7214180651f268ca31d3a
ycm666 <ycm666@naver.com>
2026-01-10 13:21:35 +0900

    third commit

---
+++ src/main/java/action/HelloAction.java
@@ -0,0 +1,29 @@
+package action;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class TestAction
+ */
+@WebServlet("/hell.do")
+public class HelloAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		response.setContentType("text/html; charset=utf-8;");
+		
+		response.getWriter().print("안녕하세요!!");
+	}
+
+}
